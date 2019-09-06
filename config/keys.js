8cf13ff341daad8e302102dba4af53067cfef18f
jimenezprogrammer <jimenezprogrammer@gmail.com>
2019-09-06 11:42:36 -0700

    with password2

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-MongoURI:   'mongodb+srv://daniel:Daniel619#@cluster0-abeyp.mongodb.net/test?retryWrites=true&w=majority'
+MongoURI:   'mongodb+srv://daniel:Daniel619%23@cluster0-abeyp.mongodb.net/test?retryWrites=true&w=majority'
 };
